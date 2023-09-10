c41a7bb6f0a21820c02a9e67d3aa1d67def0156e
Jessica Wong <wongjessica124@gmail.com>
2023-09-10 22:27:30 +0700

    latihan vue cli

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,34 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ol v-if="todos.length > 0">
+      <li v-for="todo in todos" :key="todo">{{ todo }}</li>
+    </ol>
+
+    <input type="text" v-model="todo" v-on:keyup.enter="addTodo" />
+    <button v-on:click="addTodo">Tambahkan</button>
+
+    <p v-if="todos.length > 3">Hebat!</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      todo: "",
+      todos: [],
+    };
+  },
+  methods: {
+    addTodo() {
+      if (this.todo) {
+        this.todos.push(this.todo);
+        this.todo = "";
+      }
+    },
+  },
+};
 </script>
 
 <style>
@@ -21,8 +36,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 20px;
 }
 </style>
